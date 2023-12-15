c21c90331015fc5096710954c9bc062fe9098f99
Doisaac <douglasbarrera95@gmail.com>
2023-12-15 16:43:14 -0600

    Filling a form and sending the information to a database

---
+++ databases/java-sql/form/src/main/java/com/doisaac/model/ConnecionToDB.java
@@ -0,0 +1,32 @@
+package com.doisaac.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class ConnecionToDB {
+    
+    /* Variables */
+    protected static final String URL = "jdbc:mysql://localhost:3306/forms";
+    protected static final String USER = "root";
+    protected static final String PASSWORD = "123456";
+    /* End variables */
+    
+ 
+    //Creates the connection to the database
+    public Connection getConnection() {
+
+        Connection conn = null;
+
+        try {
+
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "An error has occurred: " + ex);
+        }
+
+        return conn;
+    }
+    
+}
